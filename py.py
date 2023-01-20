37283e067fd20c01d806746c09f4360fcfd9f97f
Baoze Lin <linofbaoze@outlook.com>
2023-01-20 15:27:51 -0500

    push

--- py.py
+++ py.py
@@ -1,8 +1,15 @@
 #you start here
-def calcTax(price: int):
-    return price*0.13
+print("here")
 
 
 
 #i start here
-print("here")
+def calc_tax_on(total_price):
+    return total_price * 0.13
+
+def calc_tax_all(total_price, province):
+    dic_prov = {"ON": 0.13, "QC": 0.14975, "MB":0.05, "NS":0.15, "BC":0.05}
+    return total_price*dic_prov[province]
+
+def breakdown_sales(total_price):
+    pass
