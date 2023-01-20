a2630341803afe0a62b12c42484a3ff37cd4835c
HagOrMan <khagypi3@gmail.com>
2023-01-20 15:53:16 -0500

    final version

--- py.py
+++ py.py
@@ -1,26 +1,37 @@
+#you start here
 def main():
     items = input("input number of items: ")
     price = input("input price per item: ")
-    print(int(items)*int(price))
+    prov = input("input province: ")
+    grossPrice = int(price)*int(items)
+    sale = calcSales(grossPrice)
+    salesale = grossPrice-sale
+    tax = calc_tax_all(salesale, prov)
+    
+    print("gross price: " + str(grossPrice))
+    print("sale : " + str(sale))
+    
+    print("tax" + str(tax))
+    
+    
+    breakdown_sales(grossPrice)
+    
+    breakdown_sales_tax(grossPrice, prov)
+    
+    
 
 def calcSales(price):
     if price >= 1000:
-        return 0.03
+        return price*0.03
     elif price >= 5000:
-        return 0.05
+        return price*0.05
     elif price >= 7000:
-        return 0.07
+        return price*0.07
     elif price >= 10000:
-        return 0.1
+        return price*0.1
     elif price >= 15000:
-        return 0.15
+        return price*0.15
     return 0
-
-#you start here
-def calcTax(price: int):
-    return price*0.13
-
-
 
 #i start here
 def calc_tax_on(total_price):
@@ -32,8 +43,15 @@ def calc_tax_all(total_price, province):
 
 def breakdown_sales(total_price):
     stor = calcSales(total_price)
-    print("Total Price" + total_price)
-    print("Total Discount")
+    print("Total Price" + str(total_price))
+    print("Total Discount" + str(stor))
+    print("Final Price:" + str(total_price-stor))
 
+def breakdown_sales_tax(total_price, province):
+    stor = calcSales(total_price)
+    tax = calc_tax_all(total_price-stor, province)
+    print("Total Price" + str(total_price))
+    print("Total Discount" + str(stor))
+    print("Final Price After Tax:" + str(tax))
 
 main()
