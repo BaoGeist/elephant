be6cc64105feadf13aecf9b5d844f99c0e84383e
Baoze Lin <linofbaoze@outlook.com>
2023-01-20 15:53:46 -0500

    final

--- py.py
+++ py.py
@@ -1,26 +1,26 @@
+#you start here
 def main():
     items = input("input number of items: ")
     price = input("input price per item: ")
-    print(int(items)*int(price))
+    prov = input("input province: ")
+    grossPrice = int(price)*int(items)
+    sale = calcSales(grossPrice)
+    calc_tax_all(grossPrice-sale, prov)
+    print("gross price: " + grossPrice)
+    print("sale : " + sale)
 
 def calcSales(price):
     if price >= 1000:
-        return 0.03
+        return price*0.03
     elif price >= 5000:
-        return 0.05
+        return price*0.05
     elif price >= 7000:
-        return 0.07
+        return price*0.07
     elif price >= 10000:
-        return 0.1
+        return price*0.1
     elif price >= 15000:
-        return 0.15
+        return price*0.15
     return 0
-
-#you start here
-def calcTax(price: int):
-    return price*0.13
-
-
 
 #i start here
 def calc_tax_on(total_price):
@@ -33,7 +33,14 @@ def calc_tax_all(total_price, province):
 def breakdown_sales(total_price):
     stor = calcSales(total_price)
     print("Total Price" + total_price)
-    print("Total Discount")
+    print("Total Discount" + stor)
+    print("Final Price:" + total_price-stor)
 
+def breakdown_sales_tax(total_price, province):
+    stor = calcSales(total_price)
+    tax = calc_tax_all(total_price-stor, province)
+    print("Total Price" + total_price)
+    print("Total Discount" + stor)
+    print("Final Price After Tax:" + tax)
 
 main()
